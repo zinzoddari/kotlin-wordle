4a2dfdcee64839e3b803070e23bb71a335c161a1
zinzo <zinzoddari@gmail.com>
2026-01-27 14:40:51 +0900

    feat: 게임 라운드를 처리하기 위한 Round 객체 추가

---
+++ src/main/kotlin/wordle/domain/Round.kt
@@ -0,0 +1,32 @@
+package wordle.domain
+
+/**
+ * 현재 라운드를 의미합니다.
+ */
+@JvmInline
+value class Round(
+    val value: Int = 1
+) {
+    init {
+        require(0 < value) { "라운드는 1보다 커야합니다." }
+    }
+
+    /**
+     * Round 증감을 합니다.
+     *
+     * @return 현재 Round에서 1이 증감된 값
+     */
+    fun next(): Round {
+        return Round(this.value + 1)
+    }
+
+    /**
+     * 이 라운드 값이 전달된 라운드보다 큰지 여부를 반환합니다.
+     *
+     * @param round 비교 대상이 되는 라운드 값
+     * @return 이 라운드 값이 round보다 크면 true
+     */
+    fun isGreaterThanRound(round: Int): Boolean {
+        return this.value > round
+    }
+}
---
+++ src/test/kotlin/wordle/domain/RoundTest.kt
@@ -0,0 +1,69 @@
+package wordle.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class RoundTest {
+
+    @Test
+    @DisplayName("라운드는 1보다 작으면 예외가 발생합니다.")
+    fun test01() {
+        // arrange
+        val input: Int = -1
+
+        // act & assert
+        assertThatThrownBy { Round(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("라운드는 1보다 커야합니다.")
+    }
+
+    @Test
+    @DisplayName("다음 라운드의 경우, 현재 라운드의 +1이 됩니다.")
+    fun test02() {
+        // arrange
+        val round: Round = Round()
+        val expected: Round = Round(2)
+
+        // act
+        val sut: Round = round.next()
+
+        // assert
+        assertThat(sut).isEqualTo(expected)
+    }
+
+    @Nested
+    @DisplayName("현재 라운드가 입력받은 라운드보다 값이 더 큰지 판단할 때,")
+    inner class test03 {
+
+        @Test
+        @DisplayName("현재 라운드 값이 크면 true를 반환합니다.")
+        fun test01() {
+            // arrange
+            val currentRound: Round = Round(5)
+            val round: Int = 3
+
+            // act
+            val sut: Boolean = currentRound.isGreaterThanRound(round)
+
+            // assert
+            assertThat(sut).isTrue
+        }
+
+        @Test
+        @DisplayName("현재 라운드 값이 작으면 false를 반환합니다.")
+        fun test02() {
+            // arrange
+            val currentRound: Round = Round(5)
+            val round: Int = 6
+
+            // act
+            val sut: Boolean = currentRound.isGreaterThanRound(round)
+
+            // assert
+            assertThat(sut).isFalse
+        }
+    }
+}
